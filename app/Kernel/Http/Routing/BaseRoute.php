5c1ccbffbb3bd4d925a6d9f49c4eeb3db92fc242
thiago dias <thiago_dll@hotmail.com>
2020-06-17 23:50:41 -0300

    refactored baseRoute tests

--- app/Kernel/Http/Routing/BaseRoute.php
+++ app/Kernel/Http/Routing/BaseRoute.php
@@ -4,12 +4,42 @@ namespace App\Kernel\Http\Routing;
 
 abstract class BaseRoute
 {
+    /**
+    * @var array
+    */
+    private $acceptedMethods = [
+        'POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'
+    ];
+
+    /**
+     * @var string
+     */
     private $method;
+
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var string
+     */
+
     private $uri;
-    private $params = [];
-    private $callback;
-    private $middlewares = [];
+    /**
+     * @var array
+     */
+    private $arguments = [];
+
+    /**
+     * @var string|callable
+     */
+    private $handler;
+
+    private function getAcceptedMethods(): array
+    {
+        return $this->acceptedMethods;
+    }
 
     public function setName(string $name): self
     {
@@ -24,6 +54,10 @@ abstract class BaseRoute
 
     public function setMethod(string $method): self
     {
+        if (!in_array(strtoupper($method), $this->getAcceptedMethods())) {
+            throw new \InvalidArgumentException("{$method} is not a valid method");
+        }
+
         $this->method = $method;
         return $this;
     }
@@ -32,48 +66,62 @@ abstract class BaseRoute
     {
         return $this->method;
     }
-
+    
+    /**
+     * @param string
+     */
     public function setUri(string $uri): self
     {
         $this->uri = $uri;
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getUri(): string
     {
         return $this->uri;
     }
 
-    public function setParams(array $params): self
+    /**
+     * @param array
+     */
+    public function setArguments(array $arguments): self
     {
-        $this->params = $params;
+        $this->arguments = $arguments;
         return $this;
     }
 
-    public function getParams(): array
+    public function getArguments(): array
     {
-        return $this->params;
+        return $this->arguments;
     }
-
-    public function setCallback($callback): self
+    
+    /**
+     * @param string|callable
+     */
+    public function setHandler($handler): self
     {
-        if (!\is_callable($callback) || !\is_string($callback)) {
-            throw new \InvalidArgumentException('callback should be a callable or a controller@method string');
+        if (!\is_callable($handler) && !\is_string($handler)) {
+            throw new \InvalidArgumentException('handler should be a callable or a controller@method string');
         }
 
-        if(is_string($callback)) {
-
-            $pattern = '[\w]+\@[\w]+';
-
-        }
+        // if (is_string($handler)) {
+        //     $pattern = '[\w]+\@[\w]+';
+        //     throw new \InvalidArgumentException('handler is not a valid controller@action string');
+        // }
    
-        $this->callback = $callback;
+        $this->handler = $handler;
 
         return $this;
     }
 
-    public function getCallback(): mixed
+    /**
+     * @return callable|string
+     */
+    public function getHandler()
     {
-        return $this->callback;
+        return $this->handler;
     }
 }
--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -1,12 +1,10 @@
 <?php
 
-use Closure;
 use PHPUnit\Framework\TestCase;
 use App\Kernel\Http\Routing\Route;
 
 class RouteTest extends TestCase
 {
-
     /**
      * @param Route
      */
@@ -17,30 +15,74 @@ class RouteTest extends TestCase
         $this->route = new Route;
     }
 
-    public function testIfRouteNameisSet()
+    public function test_if_route_name_is_a_valid_string()
     {
         $this->route->setName('users.index');
         $this->assertEquals($this->route->getName(), 'users.index');
     }
+    
+    public function test_if_route_name_is_not_a_valid_string()
+    {
+        $this->expectException('TypeError');
+        $this->route->setName(new stdClass);
+    }
 
-    public function testIfRouteMethodIsSet()
+    public function test_if_method_is_a_valid_http_verb()
     {
         $this->route->setMethod('post');
         $this->assertEquals($this->route->getMethod(), 'post');
     }
     
-    public function testIfUriRouteIsSet()
+    public function test_if_method_is_not_a_valid_http_verb()
     {
-        $this->route->setMethod('post');
-        $this->assertEquals($this->route->getMethod(), 'post');
+        $this->expectException('InvalidArgumentException');
+        $this->route->setMethod('dance');
     }
     
-    public function testIfCallbackIsAValidCallable()
+    public function test_if_route_uri_is_valid()
     {
-        $this->route->setCallback(function() {
+        $this->route->setUri('/users/[i:id]');
+        $this->assertEquals($this->route->getUri(), '/users/[i:id]');
+    }
+    
+    public function test_if_route_uri_is_not_valid()
+    {
+        $this->expectException('TypeError');
+        $this->route->setUri(new StdClass);
+    }
+    
+    public function test_if_handler_is_a_valid_callable()
+    {
+        $this->route->setHandler(function () {
             return 1;
         });
+        $this->assertInstanceOf('Closure', $this->route->getHandler());
+    }
+    
+    public function test_if_handler_is_a_valid_controller_method_string()
+    {
+        $this->route->setHandler('DummyController@method');
+        $this->assertEquals('DummyController@method', $this->route->getHandler());
+    }
 
-        $this->assertInstanceOf(Closure::class, $this->route->getCallback());
+    public function test_if_handler_is_not_a_valid_controller_method_string()
+    {
+        $this->expectException('InvalidArgumentException');
+        $this->route->setHandler('22DummyController@22method');
+    }
+    
+    public function test_route_arguments()
+    {
+        $this->route->setArguments([
+            'id' => 22,
+            'name' => 'dummy'
+        ]);
+
+        $expected = [
+            'id' => 22,
+            'name' => 'dummy'
+        ];
+
+        $this->assertEquals($this->route->getArguments(), $expected);
     }
 }
